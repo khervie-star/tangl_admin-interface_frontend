8623e7fa8b0ee16acf0627f289267620e056a486
Kahmyl <kamilhabib556@gmail.com>
2022-04-02 16:58:33 +0100

    updating individual registration

--- src/components/Investors/ContentThree/styles.tsx
+++ src/components/Investors/ContentThree/styles.tsx
@@ -52,3 +52,68 @@ export const TelContainer = styled.div`
     }
   }
 `;
+export const Title = styled.span``;
+
+export const Confirmation = styled.div`
+  margin-bottom: 11rem;
+  ${Title} {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #43566a;
+  }
+
+  p {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #a0aaba;
+  }
+`;
+
+export const CodeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  input {
+    border: 0;
+    border-bottom: 1px solid #007AFB;
+    padding: 0.75rem 0.5rem; 
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: 5px;
+    width: 100%;
+    &:focus {
+      outline: none;
+    }
+    &:hover {
+      outline: none;
+    }
+  }
+`;
+
+
+export const InputContainer = styled.div`
+  width: 70%;
+`
+
+export const Resend = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: -3rem;
+  margin-left: 2rem;
+  span {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+    text-align: center;
+    font-feature-settings: "liga" off;
+    color: #007afb;
+    margin-left: 0.75rem;
+  }
+`
